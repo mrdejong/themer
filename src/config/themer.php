2c59c385c5188a351414929f165729172d53283b
Alexander de Jong <mrdejong89@gmail.com>
2014-09-25 18:43:58 +0200

    Removed configuration options that will not get into the first release.

--- src/config/themer.php
+++ src/config/themer.php
@@ -18,54 +18,6 @@ return array(
 	'active_theme'		=> 'default',
 
 	/**
-	 * There is a active theme listed in the database
-	 * and there is one listed here.
-	 *
-	 * Wich one will get the priority
-	 *
-	 * You can choose:
-	 *
-	 * -1 - The configuration file is the only real deal, ignore the rest.
-	 * 0 - The one in the themes database table has the priority.
-	 * 1 - The one which will be provided by an user. (In this case you 
-	 * 	   should configure the 'users' part in this configuration file). (Not implemented yet)
-	 */
-	'active_theme_priority'	=> 0,
-
-	/**
-	 * Everytheme should be packaged with a info.php file.
-	 * This file should contain all the information of
-	 * the theme.
-	 *
-	 * This configuration option tells themer what variables
-	 * are required in the info.php file.
-	 *
-	 * If the theme info file doesn't contain valid information
-	 * it won't be added to the themes stack, and thus the
-	 * theme will be ignored.
-	 *
-	 * This isn't strict in anyway, if a theme has more information
-	 * then listed here it'll be perfectly fine!
-	 */
-	'required_info'			=> array(
-		'title'					=> true, // A human readable title :)
-		'description'			=> true // A bit of information about the theme.
-	),
-
-	/**
-	 * If set to false, themer won't check the required_info option.
-	 */
-	'validate_info'			=> false,
-
-	/**
-	 * Should a theme be required to have a info file?
-	 *
-	 * !!Alert!! If this is set to false, themer won't check for
-	 * the required info parameters listed in the 'required_info' option.
-	 */
-	'require_info_file'		=> false,
-
-	/**
 	 * Themer provides a build-in ajax api to activate/deactivate themes.
 	 * It also gives functionallity to get information of themes.
 	 * 
