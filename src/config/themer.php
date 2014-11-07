f16f4947482bf38d3c2de6d98d3eaaf0f0843190
Alexander de Jong <mrdejong89@gmail.com>
2014-11-07 10:08:08 +0100

    Changed the configuration, thinking ahead to laravel 5

--- src/config/themer.php
+++ src/config/themer.php
@@ -1,5 +1,11 @@
 <?php
 
 return array(
-    'themes_path'            => app_path().'/themes',
+    /**
+     * @todo Add multi support for laravel 4 & 5
+     */
+    'themes_path'            => array(
+        'laravel4'        => app_path().'/themes',
+        'laravel5'        => base_path().'/resources/themes'
+    ),
 );
