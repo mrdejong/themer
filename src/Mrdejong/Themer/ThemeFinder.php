1e5ff3a82114b1e8b2e9dff4d1014c2f1ac8d390
Alexander de Jong <mrdejong89@gmail.com>
2016-06-05 20:12:37 +0200

    Use config function

--- src/Mrdejong/Themer/ThemeFinder.php
+++ src/Mrdejong/Themer/ThemeFinder.php
@@ -1,7 +1,5 @@
 <?php namespace Mrdejong\Themer;
 
-use Illuminate\Support\Facades\Config;
-
 use Mrdejong\Themer\Exceptions\ThemeNotFoundException;
 
 /**
@@ -11,14 +9,14 @@ class ThemeFinder implements ThemeFinderInterface {
 	/**
 	 * Takes the name of the theme and checks if the folder with that name
 	 * exists within the themes folder (@see config option themer::themer.themes_path for the location.)
-	 * 
+	 *
 	 * @param string 	$name 			The name of the theme
 	 * @return string 	The location of the theme.
 	 * @throws ThemeNotFoundException	If the folder doesn't exist.
 	 */
 	public function find($name)
 	{
-		$path = Config::get('themer::themer.themes_path');
+		$path = config('themer.themes_path');
 		$themePath = $path . '/' . $name;
 
 		if(!is_dir($themePath))
@@ -31,14 +29,14 @@ class ThemeFinder implements ThemeFinderInterface {
 
 	/**
 	 * This function looks throw the themes directory and returns all the themes.
-	 * 
-	 * This method is not tested, please don't expect the right behaviour. 
-	 * 
+	 *
+	 * This method is not tested, please don't expect the right behaviour.
+	 *
 	 * @return array|null 	The directories listed in the themes folder.
 	 */
 	public function listThemes()
 	{
-		$path = Config::get('themer::themer.themes_path');
+		$path = config('themer.themes_path');
 
 		return glob($path . '/*', GLOB_ONLYDIR);
 	}
