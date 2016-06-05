496566684c24d2ea08f150afeb022e56745e2544
Alexander de Jong <mrdejong89@gmail.com>
2016-06-05 20:10:57 +0200

    Disable routing filters
    * Filters are removed in laravel 5.
    * Remove whitespace

--- src/filters.php
+++ src/filters.php
@@ -6,20 +6,20 @@
  * This will enable the user to activate a theme per route.
  *
  * @usage
- * array('before' => 'theme:{themename}') 
+ * array('before' => 'theme:{themename}')
  */
-Route::filter('theme', function($route, $request, $name)
-{    
-    Themer::activate($name, $route);
-});
-
-Route::filter('is_ajax', function($route, $request)
-{
-	if (!Request::ajax())
-		return App::abort(404);
-});
-
-App::before(function($request)
-{
-	//Themer::boot($request, true);
-});
+// Route::filter('theme', function($route, $request, $name)
+// {
+//     Themer::activate($name, $route);
+// });
+//
+// Route::filter('is_ajax', function($route, $request)
+// {
+// 	if (!Request::ajax())
+// 		return App::abort(404);
+// });
+//
+// App::before(function($request)
+// {
+// 	//Themer::boot($request, true);
+// });
